61d547df027c4c2fddd0d06d8932e89349841177
Maryor-z <88011448+Maryor-z@users.noreply.github.com>
2022-12-17 09:19:33 +0100

    Update Car.java

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinders, String name){
+        this.cylinders = cylinders;
+        this.name = name;
+        this.engine = true;
+        this.wheels = wheels;
+    }
+
+    public String startEngine(){
+        return ("Engine "+this.name+" is starting");
+    }
+
+    public String accelerate(){
+        return ("Car "+this.name+" is accelerating");
+    }
+
+    public String brake(){
+        return ("Car "+this.name+" is braking");
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getCylinders(){
+        return cylinders;
+    }
 }
